8885952bc40ec94bfe7965cae465a752eff6cde9
camlafit <cam.lafit@azerttyu.net>
2016-08-08 18:11:54 +0200

    Follow new semver versionning
    We must follow change versioning acts in http://glpi-project.org/spip.php?breve355

--- setup.php
+++ setup.php
@@ -108,8 +108,14 @@ function plugin_version_racks() {
 
 // Optional : check prerequisites before install : may print errors or add to message after redirect
 function plugin_racks_check_prerequisites() {
-   if (version_compare(GLPI_VERSION,'0.90', 'lt')
-      || version_compare(GLPI_VERSION,'0.91', 'ge')) {
+   if (
+      ( version_compare(GLPI_VERSION,'1','lt') && (
+         version_compare(GLPI_VERSION,'0.90','lt') || version_compare(GLPI_VERSION,'0.91','ge')
+      )) ||
+      ( version_compare(GLPI_VERSION,'9','gt') && (
+         version_compare(GLPI_VERSION,'9.1','gt')
+      ))
+   ) {
       _e('This plugin requires GLPI >= 0.90', 'racks');
       return false;
    }
